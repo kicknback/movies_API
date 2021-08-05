eefd3ed2f96a9e22c0c6e8825a2d05369d1dac13
kicknback <joshua.borreli@gmail.com>
2021-08-05 15:17:08 -0500

    Added additional classes

---
+++ src/main/java/data/DaoFactory.java
@@ -0,0 +1,22 @@
+package data;
+
+//import data.movies.InMemoryMoviesDao;
+//import data.movies.MoviesDao;
+//import data.movies.MySqlMoviesDao;
+
+public class DaoFactory {
+
+    private static MoviesDao moviesDao;
+//    private static Config config = new Config();
+    public enum ImplType {MYSQL, IN_MEMORY}; //Notice we have two values here
+
+    public static MoviesDao getMoviesDao(ImplType implementationType){
+
+        switch(implementationType){
+            case IN_MEMORY:{ //yet we have one switch case. We'll get to that!
+                moviesDao = new InMemoryMoviesDao();
+            }
+        }
+        return moviesDao;
+    }
+}
