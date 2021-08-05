eb75c5d0c8189c5e1136337dfa1ecb89b17bfc16
kicknback <joshua.borreli@gmail.com>
2021-08-05 14:20:27 -0500

    Finished doPut and doDelete methods to complete crud

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -63,4 +63,63 @@ public class MovieServlet extends HttpServlet {
         response.setStatus(200);
     }
 
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            Movie[] movies = new Gson().fromJson(request.getReader(), Movie[].class);
+            for (Movie movie : movies) {
+                System.out.println(movie.getId());
+                System.out.println(movie.getTitle());
+                System.out.println(movie.getDirector());
+                System.out.println(movie.getPlot());
+                System.out.println(movie.getPoster());
+                System.out.println(movie.getActors());
+                System.out.println(movie.getGenre());
+                System.out.println(movie.getYear());
+                System.out.println(movie.getRating());
+                System.out.println("========================================================");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        out.println(new Gson().toJson("{message: \"Movies PUT was successful\"}"));
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            BufferedReader reader = request.getReader();
+
+            int id = new Gson().fromJson(reader, int.class);
+            System.out.println("The movie id to delete: " + id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        out.println(new Gson().toJson("{message: \"Movies DELETE was successful\"}"));
+        response.setStatus(200);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
